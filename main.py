1cd7a77b9a8f6d406b98e59603da4e510e300aa1
Guty04 <86425@sistemas.frc.utn.edu.ar>
2025-03-24 18:34:50 -0300

    Add run command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from uvicorn import run
 
 app = FastAPI()
 
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    run("main:app", host="0.0.0.0", port=8000)
